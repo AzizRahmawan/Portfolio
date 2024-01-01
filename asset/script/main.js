8991d162c754d72796d0aeaa785d07c54464fa8f
AzizRahmawan <arder058@gmail.com>
2024-01-01 14:18:30 +0700

    update hero + project

--- asset/script/main.js
+++ asset/script/main.js
@@ -3,12 +3,21 @@ const navSlide = () => {
     const burger = document.querySelector(".burger");
     const navLists = document.querySelector("nav");
     const wave = document.querySelector(".wave");
+    const navLinks = document.querySelectorAll('nav a');
 
     burger.addEventListener("click", () => {
         if (wave && !wave.classList.contains("nav-active")) {
             navLists.classList.toggle("nav-active");
             burger.classList.toggle("toggle-burger");
         }
+    });
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            // Tutup nav dan burger
+            navLists.classList.remove('nav-active');
+            burger.classList.remove('toggle-burger');
+        });
     });
 }
 
